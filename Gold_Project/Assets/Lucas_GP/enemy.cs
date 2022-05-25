2b39b875f86a7639e203dbf6dca4150e1bd2d40d
Lucas Brossier <48749809+brolucas@users.noreply.github.com>
2022-05-25 15:44:00 +0200

    Commit de transit

---
+++ Gold_Project/Assets/Lucas_GP/enemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float Enemy_Speed = 3 ;
+    public int Enemy_Damage = 1;
+    public int Enemy_HP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
