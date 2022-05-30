108b5344c8ab05d2cbb1e03245b39de0d35c9d07
Kevin Vu-Morello <kevin.morello92@gmail.com>
2022-05-30 15:43:49 +0200

    add turret placement on the grid and build manager (without price)

--- Gold_Project/Assets/Scripts/PathFinding/GridTest.cs
+++ Gold_Project/Assets/Scripts/PathFinding/GridTest.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class GridTest : MonoBehaviour
 {
     private Grid grid;
+
+    BuildManager buildManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        grid = new Grid(4, 2, 3f, new Vector3(10, 0));
+        grid = new Grid(4, 2, 3f, new Vector3(this.transform.position.x, this.transform.position.y));
+
+        buildManager = BuildManager.Instance;
     }
 
     private void Update()
     {
+        if (buildManager.GetTurretToBuild() == null)
+            return;
+
         if (Input.GetMouseButtonDown(0))
         {
             grid.SetTurret(Camera.main.ScreenToWorldPoint(Input.mousePosition));
---
+++ Gold_Project/Assets/Scripts/TurretScripts/BuildManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    #region Singleton
+    private static BuildManager instance = null;
+
+    // Game Instance Singleton
+    public static BuildManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<BuildManager>();
+
+                return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        // if the singleton hasn't been initialized yet
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+
+        instance = this;
+
+        Debug.Log(instance);
+
+    }
+    #endregion
+
+    public GameObject sniperTurret;
+
+    public GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+    public void SetTurretToBuild(GameObject turret)
+    {
+        turretToBuild = turret;
+    }
+}
