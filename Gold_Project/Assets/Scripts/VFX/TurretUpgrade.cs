886583c697ccea6f7138e55c0b45a8f1b068eb9b
hugo <hugo.monmousseau@hotmail.fr>
2022-06-01 17:24:27 +0200

    feedbacks turret upgrade

---
+++ Gold_Project/Assets/Scripts/VFX/TurretUpgrade.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretUpgrade : MonoBehaviour
+{
+    private Animator Anim;
+    [SerializeField] private ParticleSystem Particles;
+
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+    [ContextMenu("Upgrade")]
+    public void UpgradeTurret()
+    {
+        Anim.SetBool("upgrading", true);
+    }
+    public void FinUpdate()
+    {
+        Anim.SetBool("upgrading", false);
+
+    }
+    public void UpgradeParticles()
+    {
+
+        Instantiate(Particles, transform.parent.GetComponent<Transform>().position, Quaternion.identity);
+    }
+}
