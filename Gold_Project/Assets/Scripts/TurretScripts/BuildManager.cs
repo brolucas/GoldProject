817cdf3f1d8de811d11af6104eecef3ac5ad70dc
Cryp3rone <Manach.Nicolas.mn@gmail.com>
2022-06-10 10:56:46 +0200

    fix node in buildmanager && wavespawners

--- Gold_Project/Assets/Scripts/TurretScripts/BuildManager.cs
+++ Gold_Project/Assets/Scripts/TurretScripts/BuildManager.cs
@@ -86,7 +86,8 @@ public class BuildManager : MonoBehaviour
 
             GameObject newTurret = Instantiate(turretPrefab, position, Quaternion.identity);
             Pathfinding.Instance.GetGrid().GetXY(position, out int x, out int y);
-            Pathfinding.Instance.GetNode(x, y).SetIsWalkable(false);
+            Pathfinding.Instance.GetNode(x, y).isTurret = newTurret;
+            Pathfinding.Instance.GetNode(x, y).isUsed = true;
 
             if (cellSize > 0)
             {
