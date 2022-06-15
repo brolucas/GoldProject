9a1095f177c3837d47c3d4dd21e598c6ca88bbca
hugo <hugo.monmousseau@hotmail.fr>
2022-06-15 12:24:14 +0200

    canalisateur+ generator + linerenderer

--- Gold_Project/Assets/Scripts/VFX/Canalisateur.cs
+++ Gold_Project/Assets/Scripts/VFX/Canalisateur.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Canalisateur : MonoBehaviour
 {
     [SerializeField] int charge;
+    [SerializeField] bool tir;
+    [SerializeField] ParticleSystem particle;
+    [SerializeField] GameObject Electricity;
     private Animator anim;
 
     void Start()
@@ -15,5 +18,14 @@ public class Canalisateur : MonoBehaviour
     void Update()
     {
         anim.SetInteger("canalisation", charge);
+        if (tir)
+        {
+            tir = false;
+            ParticleSystem bolt = Instantiate(particle,new Vector3( transform.position.x , transform.position.y , transform.position.z -1), Quaternion.Euler(0, 0, 0));
+            bolt.transform.localScale = new Vector3(.25f + (float)charge/4, .25f + (float)charge / 4, .25f + (float)charge / 4);
+            GameObject Projectile = Instantiate(Electricity, transform.position, Quaternion.Euler(0, 0, 0));
+            Projectile.transform.localScale = new Vector3(.25f + (float)charge / 10, .25f + (float)charge / 10, .25f + (float)charge / 10);
+            charge = 0;
+        }
     }
 }
