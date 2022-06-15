cb4e81665f3d38a9b2b01f003084b37fbcac2e39
hugo <hugo.monmousseau@hotmail.fr>
2022-06-15 17:28:59 +0200

    scene ++

--- Gold_Project/Assets/Scripts/VFX/Generator.cs
+++ Gold_Project/Assets/Scripts/VFX/Generator.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] GameObject projecile;
+    [SerializeField] float delais = 2;
+    bool cd = true;
     // Update is called once per frame
     void Update()
     {
-        
+        if (cd)
+        {
+            StartCoroutine(Delais());
+            cd = false;
+        }
+    }
+    IEnumerator Delais()
+    {
+        yield return new WaitForSeconds(delais);
+        cd = true;
+        Instantiate(projecile, transform.position, Quaternion.Euler(0, 0, 0));
     }
 }
