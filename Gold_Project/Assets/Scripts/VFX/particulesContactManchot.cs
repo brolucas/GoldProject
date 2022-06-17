7b536d544ac0bccc3bf928f6d187c50a7a79e3ec
hugo <hugo.monmousseau@hotmail.fr>
2022-06-17 16:41:00 +0200

    particules qui s arretent pile poile

---
+++ Gold_Project/Assets/Scripts/VFX/particulesContactManchot.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particulesContactManchot : MonoBehaviour
+{
+    void OnParticleTrigger()
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+
+        // particles
+        List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+        List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
+
+        // get
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        int numExit = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+
+        // iterate
+        for (int i = 0; i < numEnter; i++)
+        {/*
+            ParticleSystem.Particle p = enter[i];
+            p.startColor = new Color32(255, 0, 0, 255);
+            enter[i] = p;*/
+            Debug.Log(i);
+        }
+        for (int i = 0; i < numExit; i++)
+        {
+            ParticleSystem.Particle p = exit[i];
+            p.startColor = new Color32(0, 255, 0, 255);
+            exit[i] = p;
+        }
+
+        // set
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+    }
+}
---
+++ Gold_Project/Assets/Scripts/VFX/rangeLifeTimeParticles.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rangeLifeTimeParticles : MonoBehaviour
+{
+    public Transform target;
+    [SerializeField] private float rangeTarget;
+    [SerializeField] float lifeTime;
+    private ParticleSystem particles;
+    [SerializeField] float y;
+    
+
+    private void Update()
+    {
+        particles = GetComponent<ParticleSystem>();
+        lifeTime = particles.startSpeed;
+        var main = particles.main;
+        main.startLifetime = Pythagore(transform.position.x - target.transform.position.x, transform.position.y - target.transform.position.y) * particles.startSpeed /y;
+        
+    }
+
+    private float Pythagore(float nb1, float nb2)
+    {
+        return Mathf.Sqrt((nb1 * nb1) + (nb2 * nb2));
+    }
+
+}
